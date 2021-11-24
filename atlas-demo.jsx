d744650e3107e61f779cbda530d589eb1484d723
nikitam7 <85907233+nikitam7@users.noreply.github.com>
2021-11-24 11:52:33 -0800

    Add files via upload

---
+++ atlas-demo.jsx
@@ -0,0 +1,6 @@
+<script type="text/javascript">
+window.TiEmbedSettings = {
+  embed: 'https://nikit.thoughtindustries.com/embed/atlas-refactor'
+};
+</script>
+<script type="text/javascript" src="https://d36ai2hkxl16us.cloudfront.net/ti-embed-widget/v2/dist/ti-embed.production.umd.js"></script>
